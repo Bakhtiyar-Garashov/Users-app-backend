c0ce363f86a7171c36c9176930d24adb1cbfa070
bakhtiyar_garashov <bakhtiyar.garashov@ut.ee>
2020-10-15 00:21:34 +0300

    final commit

--- klausApp/urls.py
+++ klausApp/urls.py
@@ -37,6 +37,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("user_api.urls")),
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/doc', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
   
 ]
