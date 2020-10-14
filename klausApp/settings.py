35e298becf407081b69baad0331043a455e5acda
bakhtiyar_garashov <bakhtiyar.garashov@ut.ee>
2020-10-14 23:02:19 +0300

    last

--- klausApp/settings.py
+++ klausApp/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
-django_heroku.settings(locals())
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -140,3 +140,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 
+django_heroku.settings(locals())
